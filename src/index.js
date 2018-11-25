ec4fa155dcfc820ea688b1ec64aac7498dc90910
Antonio Vivace <avivace4@gmail.com>
2018-11-25 17:59:51 +0100

    Use sass-loader and rule on sass files to load Bulma

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 console.log('Here we are')
+import 'bulma'
 import './stylus/main.styl'
 import './fonts/fantasque/FantasqueSansMono-Regular-decl.css'
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ const config = {
   module: {
     rules: [
       {
+            test: /\.sass$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        },
+      {
         test: /\.styl$/,
         use: [
           'style-loader',
