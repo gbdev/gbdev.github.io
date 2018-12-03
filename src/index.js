6965c3c807d3e1efc183e467ba6d9d648b9bb127
Antonio Vivace <avivace4@gmail.com>
2018-12-03 23:01:16 +0100

    Import Game Boy SCSS animation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log('Here we are')
 import 'bulma'
-import './stylus/main.styl'
+import './styles/main.styl'
 import 'typeface-chakra-petch'
 import 'typeface-cabin'
 import 'typeface-cairo'
+import './styles/gameboy.scss'
