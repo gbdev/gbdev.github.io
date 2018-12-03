2064ab6c5e50a8fa59bf8b0fe38234a263b725b0
Antonio Vivace <avivace4@gmail.com>
2018-12-03 23:38:53 +0100

    Remove Cabin font, previously used for the Game Boy logo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ console.log('Here we are')
 import 'bulma'
 import './styles/main.styl'
 import 'typeface-chakra-petch'
-import 'typeface-cabin'
 import 'typeface-cairo'
 import './styles/gameboy.scss'
 
