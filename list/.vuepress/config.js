d9ae9f406c102c3e853264e8f60acb62eb1c158e
Antonio Vivace <avivace4@gmail.com>
2019-10-14 11:09:50 +0200

    Add and enable favicons by @LIJI32

--- list/.vuepress/config.js
+++ list/.vuepress/config.js
@@ -1,6 +1,11 @@
 // .vuepress/config.js
 module.exports = {
   	title: 'gbdev',
+    description: 'game boy development scene',
+    head: [
+      ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png"}],
+      ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png"}],
+    ],
  //base: "/list",
   themeConfig: {
     sidebar: [
