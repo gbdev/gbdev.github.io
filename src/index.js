d5387bebe2d89b2fc0b2851340c81f7cb066cf58
Antonio Vivace <avivace4@gmail.com>
2019-10-14 11:11:38 +0200

    Remove old website

--- src/index.js
+++
@@ -1,8 +0,0 @@
-console.log('Here we are')
-import 'bulma'
-import './styles/main.styl'
-import 'typeface-chakra-petch'
-import 'typeface-cairo'
-import './styles/gameboy.scss'
-
-import './assets/gb-pocket-logo.png';
