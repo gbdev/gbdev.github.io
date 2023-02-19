4f112040e9d11246ec57d7f51dfe1c593f48f7ba
Antonio Vivace <avivace4@gmail.com>
2023-02-19 01:35:09 +0100

    config: remove some links and format js

--- website/.vuepress/config.js
+++ website/.vuepress/config.js
@@ -2,79 +2,98 @@
 
 //const { path } = require("@vuepress/utils");
 
-import  markdownRawPlugin  from 'vite-raw-plugin';
-import { defaultTheme } from 'vuepress'
-import { viteBundler } from 'vuepress'
-import shiki from 'vuepress-plugin-shiki'
+import markdownRawPlugin from "vite-raw-plugin";
+import { defaultTheme } from "vuepress";
+import { viteBundler } from "vuepress";
+import shiki from "vuepress-plugin-shiki";
 
 module.exports = {
-    plugins: [
-        shiki({ 'langs': ['asm'] }),
+  plugins: [shiki({ langs: ["asm"] })],
+  title: "gbdev.io",
+  description: "game boy development scene",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicons/favicon-32x32.png",
+      },
     ],
-    title: 'gbdev',
-    description: 'game boy development scene',
-    head: [
-        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png" }],
-        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png" }],
-        // Default meta values, frontmatter the pages will overwrite this
-        ['meta', { property: 'og:site_name', content: 'gbdev.io' }],
-        ['meta', { name: 'twitter:card', content: 'summary' }],
-        ['meta', { name: 'twitter:site', content: '@gbdev0' }],
-        ['meta', { name: 'og:image', content : 'https://gbdev.io/images/gbinternals.png'}]
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicons/favicon-16x16.png",
+      },
     ],
-    //base: "/list",
-    theme: defaultTheme({
-        lastUpdated: false,
-        contributors: false,
-        editLink: true,
-        docsRepo: "https://github.com/gbdev/gbdev.github.io",
-        docsBranch: "dev",
-        docsDir: "website",
-        footer: 'test',
-        navbar: [
-            { link: 'https://hh.gbdev.io', text: 'Games' },
-            { link: 'https://gbdev.io/pandocs', text: 'Pan Docs' },
-            { link: 'https://www.getrevue.co/profile/gbdev', text: 'Newsletter' },
-            {
-                text: 'Community',
-                link: '/',
-                collapsable: false,
-                sidebarDepth: 1,
-                children: [
-                    { link: '/chat', text: 'Chat' },
-                    { link: '/contribute', text: 'Contribute' },
-                    {
-                        link: '/gbcompo21',
-                        text: 'GB Competition 2021',
-                    }
-                ]
-            },
-            { link: '/resources', text: 'Resources' },
-            {
-                text: 'Guides',
-                children: [
-                    { link: '/guides/tools', text: 'Choosing development tools' },
-                    { link: '/guides/asmstyle', text: 'ASM Style recomendations' },
-                    { link: '/guides/lyc_timing', text: 'The Timing of LYC STAT Handlers' },
-                    { link: '/guides/deadcscroll', text: 'Dead C Scroll' },
-                    { link: 'https://eldred.fr/blog/2022/05/22/prehistorik', text: 'The wonders of Prehistorik Man' },
-                    { link: '/guides/dma_hijacking', text: 'DMA Hijacking' },
-                    { link: 'https://eldred.fr/gb-asm-tutorial', text: 'GB ASM Programming Guide' }
-                ]
-            }
-        ]
-    }),
+    // Default meta values, frontmatter the pages will overwrite this
+    ["meta", { property: "og:site_name", content: "gbdev.io" }],
+    ["meta", { name: "twitter:card", content: "summary" }],
+    ["meta", { name: "twitter:site", content: "@gbdev0" }],
+    [
+      "meta",
+      { name: "og:image", content: "https://gbdev.io/images/gbinternals.png" },
+    ],
+  ],
+  //base: "/list",
+  theme: defaultTheme({
+    lastUpdated: false,
+    contributors: false,
+    editLink: true,
+    docsRepo: "https://github.com/gbdev/gbdev.github.io",
+    docsBranch: "dev",
+    docsDir: "website",
+    footer: "test",
+    navbar: [
+      { link: "https://hh.gbdev.io", text: "Games" },
+      { link: "https://gbdev.io/pandocs", text: "Pan Docs" },
+      {
+        text: "Community",
+        link: "/",
+        collapsable: false,
+        sidebarDepth: 1,
+        children: [
+          { link: "/chat", text: "Chat" },
+          { link: "/contribute", text: "Contribute" },
+          {
+            link: "/gbcompo21",
+            text: "GB Competition 2021",
+          },
+        ],
+      },
+      { link: "/resources", text: "Resources" },
+      {
+        text: "Guides",
+        children: [
+          { link: "/guides/tools", text: "Choosing development tools" },
+          { link: "/guides/asmstyle", text: "ASM Style recomendations" },
+          {
+            link: "/guides/lyc_timing",
+            text: "The Timing of LYC STAT Handlers",
+          },
+          { link: "/guides/deadcscroll", text: "Dead C Scroll" },
+          {
+            link: "https://eldred.fr/blog/2022/05/22/prehistorik",
+            text: "The wonders of Prehistorik Man",
+          },
+          { link: "/guides/dma_hijacking", text: "DMA Hijacking" },
+        ],
+      },
+    ],
+  }),
 
-    bundler: viteBundler({
-        viteOptions: {
-            plugins: [
-                markdownRawPlugin({ fileRegex: /\.asm$/ }),
-            ],
-            resolve: {
-                alias: {
-                    '@': __dirname, // Alias to the `.vuepress` folder
-                },
-            },
+  bundler: viteBundler({
+    viteOptions: {
+      plugins: [markdownRawPlugin({ fileRegex: /\.asm$/ })],
+      resolve: {
+        alias: {
+          "@": __dirname, // Alias to the `.vuepress` folder
         },
-    }),
-}
+      },
+    },
+  }),
+};
