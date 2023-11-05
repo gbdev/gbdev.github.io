0d1dbb1302af0f76aa875796dc82627a64c341e0
GitHub Action <community@gbdev.io>
2023-11-05 16:21:54 +0000

    Update build

---
+++ assets/2023-11-04-sc.html-73e957ae.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-5f5d6e7d","path":"/meetings/2023-11-04-sc.html","title":"Steering Committee - November 2023","lang":"en-US","frontmatter":{"sidebar":false},"headers":[],"git":{},"filePathRelative":"meetings/2023-11-04-sc.md"}');export{e as data};
---
+++ assets/meetings.html-51bd8c78.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-45972830","path":"/meetings.html","title":"Meetings minutes","lang":"en-US","frontmatter":{"sidebar":false},"headers":[],"git":{},"filePathRelative":"meetings.md"}');export{e as data};
