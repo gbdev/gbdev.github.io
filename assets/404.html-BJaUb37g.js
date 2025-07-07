3c9d4d8dfed3b744d0981ccee212368b7e318b64
GitHub Action <community@gbdev.io>
2025-07-07 22:58:46 +0000

    Update build

--- assets/404.html-BJaUb37g.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,d as n,o as a}from"./app-neDDUpp1.js";const l={};function r(s,t){return a(),o("div",null,t[0]||(t[0]=[n("p",null,"404 Not Found",-1)]))}const d=e(l,[["render",r]]),_=JSON.parse('{"path":"/404.html","title":"","lang":"en-US","frontmatter":{"layout":"NotFound"},"headers":[],"git":{},"filePathRelative":null}');export{d as comp,_ as data};
